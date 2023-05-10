e6086cc78545d29e54abb4f90f5a586e465efc5a
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-05-10 21:45:14 +0300

    b53

--- src/train.cpp
+++ src/train.cpp
@@ -1,57 +1,56 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-
 Train::Train() : first(nullptr), countOp(0) {}
 void Train::addCage(bool light) {
-	if (!first) {
-		first = new Cage;
-		first->light = light;
-		first->next = nullptr;
-		first->prev = nullptr;
-	} else if (first->next == nullptr) {
-		Cage* item = new Cage;
-		item->light = light;
-		item->prev = first;
-		item->next = first;
-		first->next = item;
-		first->prev = item;
-	} else {
-		Cage* item = new Cage;
-		item->light = light;
-		item->prev = first->prev;
-		first->prev->next = item;
-		item->next = first;
-		first->prev = item;
-	}
+    if (!first) {
+        first = new Cage;
+        first->light = light;
+        first->next = nullptr;
+        first->prev = nullptr;
+    } else if (first->next == nullptr) {
+        Cage* item = new Cage;
+        item->light = light;
+        item->prev = first;
+        item->next = first;
+        first->next = item;
+        first->prev = item;
+    } else {
+        Cage* item = new Cage;
+        item->light = light;
+        item->prev = first->prev;
+        first->prev->next = item;
+        item->next = first;
+        first->prev = item;
+    }
 }
 int Train::getLength(){
-	int lenght = 0;
-	int curStep = 0;
-	Cage* item = first;
-	if (!item->light) {
-		item->light = true;
-	}
-	while (true) {
-		while (true) {
-			item = item->next;
-			curStep++;
-			countOp++;
-			if (item->light) {
-				item->light = 0;
-				break;
-			}
-		}
-		lenght = curStep;
-		while (curStep != 0) {
-			item = item->prev;
-			countOp++;
-			curStep--;
-		}
-		if (!item->light)
-			return lenght;
-	}
+    int lenght = 0;
+    int curStep = 0;
+    Cage* item = first;
+    if (!item->light) {
+        item->light = true;
+    }
+    while (true) {
+        while (true) {
+            item = item->next;
+            curStep++;
+            countOp++;
+            if (item->light) {
+                item->light = 0;
+                break;
+            }
+        }
+        lenght = curStep;
+        while (curStep != 0) {
+            item = item->prev;
+            countOp++;
+            curStep--;
+        }
+        if (!item->light)
+            return lenght;
+    }
 }
 int Train::getOpCount() {
-	return countOp;
+    return countOp;
 }
 
