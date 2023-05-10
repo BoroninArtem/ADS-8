c9c0756f2638d109a7ba5f6889aafa2368c91db9
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-05-10 21:26:58 +0300

    b53

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,59 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() : first(nullptr), countOp(0) {}
+void Train::addCage(bool light) {
+	if (!first) {
+		Cage* item = new Cage;
+		item->light = light;
+		item->next = nullptr;
+		item->prev = nullptr;
+	} else if (first->next == nullptr) {
+		Cage* item = new Cage;
+		item->light = light;
+		item->prev = first;
+		item->next = first;
+		first->next = item;
+		first->prev = item;
+	} 
+  else {
+		Cage* item = new Cage;
+		item->light = light;
+		item->prev = first->prev;
+		first->prev->next = item;
+		item->next = first;
+		first->prev = item;
+	}
+}
+int Train::getLength(){
+	int lenght = 0;
+	int curStep = 0;
+	Cage *item = new Cage;
+	if (!item->light) {
+		item->light = true;
+	}
+	while (true) {
+		while (true) {
+			item = item->next;
+			curStep++;
+			countOp++;
+			if (item->light) {
+				item->light = 0;
+				break;
+			}
+			lenght = curStep;
+			while (curStep != 0) {
+				item = item->prev;
+				countOp++;
+				curStep--;
+			}
+			if (!item->light) {
+				return lenght;
+			}
+		}
+	}
+}
+int Train::getOpCount() {
+	return countOp;
+}
+
